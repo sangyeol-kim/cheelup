e9966a138ecea98c7b9fb9d01562f59c3ccc6026
Eunjin Kim <dmswlskim@naver.com>
2018-08-24 15:10:05 +0000

    nav노력

--- db/schema.rb
+++ db/schema.rb
@@ -38,17 +38,6 @@ ActiveRecord::Schema.define(version: 20180824073413) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "color"
-    t.datetime "start_time"
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
